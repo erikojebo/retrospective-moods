de071e3e3148e30d1e9d3750bbcc1eea4c2c9213
Erik Öjebo <erik.ojebo@gmail.com>
2012-12-02 12:28:46 +0100

    Added animation for images when selected

--- client/moods.js
+++ client/moods.js
@@ -1,19 +1,22 @@
+function getMaximizedImageSize () {
+	return Math.min($(window).width(), $(window).height());
+}
+
 $(document).ready(function () {
 
     $(window).on('resize', function () {
 
-        var min = Math.min($(window).width(), $(window).height());
+        var min = getMaximizedImageSize();
 
         $("#selectedImage").css({
-            width     : min,
-            height    : min
+            width : min,
+            height : min
         });
     });
 
     $(window).trigger('resize');
 
     $("#selectedImage").click(function () {
-        console.log("click");
 	    $("#overlay").hide();
         $("#content").show();
     });
@@ -22,8 +25,27 @@ $(document).ready(function () {
 
         var imageSource = $(this).attr("src");
         
+        $("#selectedImage").css({
+            width: $(this).width(),
+            height: $(this).height(),
+            left: $(this).position().left,
+            top: $(this).position().top
+        });
+
         $("#selectedImage").attr("src", imageSource);
         $("#overlay").show();
         $("#content").hide();
+
+        var size = getMaximizedImageSize();
+        var left = $(window).width() / 2.0 - size / 2.0;
+        var top = $(window).height() / 2.0 - size / 2.0;
+
+
+        $("#selectedImage").animate({
+            top: top + 'px',
+            left: left + 'px',
+            width: size + "px",
+            height: size + "px",
+        }, 250);
     });
 });
